134a2bb53030dd2807dc2af9d021f7fbfe5fe945
Otro <waichatt@gmail.com>
2025-04-21 22:18:50 -0300

    analiticas anadidas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { Roboto, Montserrat, Poppins } from 'next/font/google';
+import { Roboto } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { LanguageProvider } from "@/components/language-provider"
@@ -13,7 +14,7 @@ export const metadata: Metadata = {
   title: "WaiChatt",
   description: "Revoluciona tu comunicación con agentes de IA integrados con WhatsApp",
   keywords: "WhatsApp, IA, chatbot, automatización, ventas, atención al cliente, agente IA",
-    generator: 'v0.dev'
+  generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="">
           <LanguageProvider>{children}</LanguageProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
